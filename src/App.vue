9c7c6cc1b77dca3932f34bf43601a16d01919b6b
Elena Brazhnikova <lenovosibirsk@mail.ru>
2023-06-23 22:18:41 +0700

    start project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <main>
+    <header>
+      <img
+        src="./assets/pinia-logo.svg"
+        alt=""
+      />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script></script>
+
+<style scoped></style>
